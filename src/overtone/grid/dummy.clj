b3a5464bef1cf4347979749d5078f8e2e08cb309
Philip Potter <philipgpotter@gmail.com>
2012-01-11 12:20:33 +0100

    Let's have a DummyGrid defrecord as well for completeness

--- src/overtone/grid/dummy.clj
+++ src/overtone/grid/dummy.clj
@@ -1,20 +1,23 @@
 (ns overtone.grid.dummy
   (:use [overtone.grid]))
 
+(defrecord DummyGrid [n-cols n-rows]
+  Grid
+  ;; might be nice to be able to manually trigger button events
+  ;; for testing purposes rather than just ignoring on-action
+  (on-action [this f group name]
+    nil)
+  (dimensions [this]
+    [n-cols n-rows])
+  (set-all-leds [this colour]
+    nil)
+  (led-set [this x y colour]
+    nil)
+  (led-frame [this rows]
+    nil))
+
 (defn make-dummy
   ([]
-     (make-dummy 8 8))
+     (DummyGrid. 8 8))
   ([x y]
-     (reify Grid
-       ;; might be nice to be able to manually trigger button events
-       ;; for testing purposes rather than just ignoring on-action
-       (on-action [this f group name]
-         nil)
-       (dimensions [this]
-         [x y])
-       (set-all-leds [this colour]
-         nil)
-       (led-set [this x y colour]
-         nil)
-       (led-frame [this rows]
-         nil))))
+     (DummyGrid. x y)))
