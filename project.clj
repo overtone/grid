920f54e0f72d16b496aa2d4f4716e08a672f48d6
Sam Aaron <samaaron@gmail.com>
2012-03-23 18:48:33 +0000

    update protocol

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject overtone/device/grid "0.0.2"
+(defproject overtone/device.grid "0.0.3"
   :description "An abstraction for button-grid based controllers"
   :url "https://github.com/overtone/overtone.device.grid"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.3.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
--- src/overtone/device/grid.clj
+++ src/overtone/device/grid.clj
@@ -1,23 +1,31 @@
 (ns overtone.device.grid)
 
 (defprotocol Grid
-  "A generic representation of a grid of buttons which are also lights."
+  "A generic representation of a grid of buttons which are also
+  lights such as a monome or launchpad."
   (on-action [this key f]
     "Registers a callback fn, with the unique key to be called
-     whenever one of the buttons is pressed or released.
-     The fn should take three args: [type x y], where type is :press
-     or :release, and x and y are the coords of the button.")
+     whenever one of the buttons is pressed or released.  The fn
+     should take three args: [action x y], where action is a keyword
+     from the set #{:pressed :released} and x and y are the coords of
+     the button being pressed.")
+  (remove-action-handler [this key]
+    "Removes the action handler associated with the specified key.")
+  (action-handlers [this]
+    "Returns a seq of keys for each registered handler.")
   (led-set [this x y colour]
     "Set the LED at position [x y] to the given colour. Color 0 is
     off, > 1 represents a specific palette colour. Subject to
     change.")
-  (set-all-leds [this colour]
+  (led-set-all [this colour]
     "Sets all LEDs to a single colour or off if 0.")
   (led-frame [this leds]
-    "Update the entire field of LEDs. The rows arg is a map of coords to colour values, the same as in led-set.
+    "Update the entire field of LEDs. The rows arg is a map of coords
+    to colour values, the same as in led-set.
 
-     eg, if you have a grid 2 columns by 2 rows, you could update all of the LEDs like this:
+     eg, if you have a grid 2 columns by 2 rows, you could update all
+     of the LEDs like this:
        (led-frame grid {[0 0] 1 [1 0] 0
                         [0 1] 0 [1 1] 1})
 
-     Any unspecified coords will default to 0."))
+     Any unspecified coords should default to 0."))
