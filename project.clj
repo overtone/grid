e5b0190e673404d89f3dc8ebd5b144623aecd405
Sam Aaron <samaaron@gmail.com>
2012-03-25 20:34:19 +0100

    Add width and height, remove-all-handlers and slightly rename some methods.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/device.grid "0.0.3"
-  :description "An abstraction for button-grid based controllers"
+(defproject overtone/device.grid "0.0.4"
+  :description "An abstraction for button-grid based controllers such as the monome and launchpad."
   :url "https://github.com/overtone/overtone.device.grid"
   :dependencies [[org.clojure/clojure "1.3.0"]])
--- src/overtone/device/grid.clj
+++ src/overtone/device/grid.clj
@@ -3,15 +3,28 @@
 (defprotocol Grid
   "A generic representation of a grid of buttons which are also
   lights such as a monome or launchpad."
+  (width [this]
+    "Returns the number of elements of the grid width-wise. This value
+    is not expected to change.")
+  (height [this]
+    "Returns the number of elements of the grid height-wise. This
+    value is not expected to change.")
   (on-action [this key f]
     "Registers a callback fn, with the unique key to be called
-     whenever one of the buttons is pressed or released.  The fn
-     should take three args: [action x y], where action is a keyword
-     from the set #{:pressed :released} and x and y are the coords of
-     the button being pressed.")
-  (remove-action-handler [this key]
-    "Removes the action handler associated with the specified key.")
-  (action-handlers [this]
+     whenever one of the buttons is pressed or released. If a handler
+     has already been registered with the specified key, replace the
+     old handler with this new one.
+
+     The handler fn should take three args: [action x y], where action
+     is a keyword from the set #{:pressed :released} and x and y are
+     the coords of the button being pressed.")
+  (remove-handler [this key]
+    "Removes the action handler associated with the specified
+    key. Returns true if removal was successful, nil otherwise.")
+  (remove-all-handlers [this]
+    "Removes all action handlers asscociated with all keys. Returns
+    the number of handlers removed, nil if no handlers were removed.")
+  (handler-keys [this]
     "Returns a seq of keys for each registered handler.")
   (led-set [this x y colour]
     "Set the LED at position [x y] to the given colour. Color 0 is
